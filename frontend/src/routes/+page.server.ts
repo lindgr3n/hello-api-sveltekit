9d3ee21c8c94c2027f766be1f26886f5771829f9
Johan Lindgren <Johan.Lindgren@xlent.se>
2023-04-06 08:50:16 +0200

    fix: test

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,7 +1,16 @@
 import type { PageServerLoad, Actions } from './$types';
 
-export const load: PageServerLoad = async function ({ fetch }) {
-	const response = await fetch('http://localhost:3333/users'); // Because we are on the same domin localhost in this case the cookies is passed and auth works.
+export const load: PageServerLoad = async function ({ fetch, cookies }) {
+	const cookieString = cookies
+		.getAll()
+		.map((c) => `${c.name}=${c.value}`)
+		.join('; ');
+	console.log('STORED COOKIES', cookieString);
+
+	const response = await fetch('http://localhost:3333/users', {
+		credentials: 'include',
+		headers: { Cookie: cookieString }
+	}); // Because we are on the same domin localhost in this case the cookies is passed and auth works.
 	// Need to have adonis and sveltekit on the same domain. e.g. my-app.com and adonis is sub for that
 	const data = await response.json();
 	console.log('DATA FROM ADONIS', data);
